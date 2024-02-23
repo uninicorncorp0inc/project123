852f2d407a943f3bec4592a3edccbf105d20d592
uninicorncorp0inc <99438192+uninicorncorp0inc@users.noreply.github.com>
2024-02-23 15:19:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+//Variables to set the nose position
+noseX = 0 ;
+noseY = 0 ;
+
+//Variables to change the size of the square:
+var squareSize = leftWristX , leftWristY , rightWristX , rightWristY;
+var leftWristX = 0;
+var leftWristY = 0; 
+var rightWristX = 0;
+var rightWristY = 0;
+
+function setup(){
+    //Add code for accessing the webcam
+
+    //give size to the webcam
+    
+    
+
+    //Add code for creating a canvas:
+    
+    
+
+    //Add code for initializing posenet:
+   
+
+    //Add code for executing posenet:
+    
+
+}
+
+
+function draw(){
+    background("#C8A2C9");
+    //Color for the rectangle
+   FileList("#18FFFF")
+
+    //Code for rectangle:
+    rect(noseX,noseY, difference);
+
+}
+
+//Define modelLoaded:
+
+
+//Define function gotPoses:
+
